1a9752df81aff04ce0d04393534609fc85ffa996
kiarash14 <omidbakhsh22@gmail.com>
2016-02-27 20:47:18 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,15 +1,104 @@
 do
- function run(msg, matches)
  
- local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
- local fuses = '!printf user#id' .. msg.from.id
+function pairsByKeys(t, f)
+      local a = {}
+      for n in pairs(t) do table.insert(a, n) end
+      table.sort(a, f)
+      local i = 0      -- iterator variable
+      local iter = function ()   -- iterator function
+        i = i + 1
+        if a[i] == nil then return nil
+        else return a[i], t[a[i]]
+        end
+      end
+      return iter
+    end
+ 
+-- Returns true if is not empty
+local function has_usage_data(dict)
+  if (dict.usage == nil or dict.usage == '') then
+    return false
+  end
+  return true
+end
+ 
+-- Get commands for that plugin
+local function plugin_help(name,number,requester)
+  local plugin = ""
+  if number then
+    local i = 0
+    for name in pairsByKeys(plugins) do
+      if plugins[name].hidden then
+        name = nil
+      else
+        i = i + 1
+        if i == tonumber(number) then
+          plugin = plugins[name]
+        end
+      end
+    end
+  else
+    plugin = plugins[name]
+    if not plugin then return nil end
+  end
+ 
+    local text = ""
+    if (type(plugin.usage) == "table") then
+      for ku,usage in pairs(plugin.usage) do
+          if ku == 'user' then -- usage for user
+              if (type(plugin.usage.user) == "table") then
+                  for k,v in pairs(plugin.usage.user) do
+                      text = text..v..'\n'
+                  end
+              elseif has_usage_data(plugin) then -- Is not empty
+                  text = text..plugin.usage.user..'\n'
+              end
+          elseif ku == 'moderator' then -- usage for moderator
+              if requester == 'moderator' or requester == 'admin' or requester == 'sudo' then
+                  if (type(plugin.usage.moderator) == "table") then
+                      for k,v in pairs(plugin.usage.moderator) do
+                          text = text..v..'\n'
+                      end
+                  elseif has_usage_data(plugin) then -- Is not empty
+                      text = text..plugin.usage.moderator..'\n'
+                  end
+              end
+          elseif ku == 'admin' then -- usage for admin
+              if requester == 'admin' or requester == 'sudo' then
+                  if (type(plugin.usage.admin) == "table") then
+                      for k,v in pairs(plugin.usage.admin) do
+                          text = text..v..'\n'
+                      end
+                  elseif has_usage_data(plugin) then -- Is not empty
+                      text = text..plugin.usage.admin..'\n'
+                  end
+              end
+          elseif ku == 'sudo' then -- usage for sudo
+              if requester == 'sudo' then
+                  if (type(plugin.usage.sudo) == "table") then
+                      for k,v in pairs(plugin.usage.sudo) do
+                          text = text..v..'\n'
+                      end
+                  elseif has_usage_data(plugin) then -- Is not empty
+                      text = text..plugin.usage.sudo..'\n'
+                  end
+              end
+          else
+              text = text..usage..'\n'
+          end
+      end
+      text = text..'______________________________\n'
+    elseif has_usage_data(plugin) then -- Is not empty
+      text = text..plugin.usage..'\n______________________________\n'
+    end
+    return text
+end
  
  
-   local text = matches[1]
-   local chat = "chat#id"..134557188   --like : local chat = "chat#id"..132557188   
-  local sends = send_msg(chat, fuse, ok_cb, false)
-  return 'Commands list :
-!kick [username|id]
+-- !help command
+local function telegram_help()
+  local text = "Umbrella Tools List:\n______________________________\n"
+ !kick [username|id]
 You can also do it by reply
 〰〰〰〰〰〰
 !ban [ username|id]
@@ -146,21 +235,68 @@ set welcome disable
 *Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
 〰〰〰〰〰〰
 *Only owner can use res,setowner,promote,demote and log commands'
+  text = text..'______________________________'
+  text = text..'\n'..'You can use < /helps > for view all items info or can use < /help (name) > for view a item info'
+  text = text..'\n'..'For see about Umbrella Bot Team, send < /ver >'
+  text = text..'\n'..' Channel: @TeleBumper'
+  return text
+end
  
- end
- end
- return {
-  
-  description = "Feedback",
  
-  usage = "!feedback message",
+-- !help all command
+local function help_all(requester)
+  local ret = ""
+  for name in pairsByKeys(plugins) do
+    if plugins[name].hidden then
+      name = nil
+    else
+      ret = ret .. plugin_help(name, nil, requester)
+    end
+  end
+  return ret
+end
+ 
+local function run(msg, matches)
+  if is_sudo(msg) then
+      requester = "sudo"
+  elseif is_admin(msg) then
+      requester = "admin"
+  elseif is_momod(msg) then
+      requester = "moderator"
+  else
+      requester = "user"
+  end
+  if matches[1] == "[!/]help" then
+    return telegram_help()
+  elseif matches[1] == "[!/]helps" then
+    return help_all(requester)
+  else
+    local text = ""
+    if tonumber(matches[1])  then
+      text = plugin_help(nil, matches[1], requester)
+    else
+      text = plugin_help(matches[1], nil, requester)
+    end
+    if not text then
+      text = telegram_help()
+    end
+    return text
+  end
+end
+ 
+return {
+  description = "Help For Command and Tools",
+  usage = {
+    "/help : view items list",
+    "/helps : view all commands and info",
+    "/help (name) : view a item commands and info",
+  },
   patterns = {
-  "^[Ff]eedback (.*)$"
- 
+    "^[!/]help$",
+    "^[!/]helps",
+    "^[!/]help (.+)"
   },
   run = run
- }
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---Ú©Ù¾ÛŒ Ø¨Ø¯ÙˆÙ† Ø°Ú©Ø± Ù…Ù†Ø¨Ø¹ Ø­Ø±Ø§Ù… Ø§Ø³Øª
+}
+ 
+end
